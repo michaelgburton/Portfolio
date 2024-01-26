5ed1a58ca4dc66e9e970792533fdcc2ca13aee65
Mike Burton <michaelgburton@hotmail.com>
2024-01-26 14:44:43 -0330

    Adding generator test as well as some additional git necessaries

--- practice/python/loops/loops/for_loop.py
+++ practice/python/loops/loops/for_loop.py
@@ -1,6 +1,20 @@
 def sum_for_loop(max):
     sum = 0
-    for i in 1..max:
+    for i in range(1, max+1):
         sum += i
 
     return sum
+
+def generate_sequence(max):
+    sequence = []
+    for i in get_sequence_generator(max):
+        sequence.append(i)
+    return sequence
+
+
+def get_sequence_generator(max):
+    i = 1
+    while i <= max:
+        yield i
+        i = i+1
+        
--- practice/python/loops/tests/for_loop_test.py
+++ practice/python/loops/tests/for_loop_test.py
@@ -1,11 +1,15 @@
 import unittest
-from loops.for_loop import sum_for_loop
+from loops.for_loop import sum_for_loop, generate_sequence
 
 class TestForLoops(unittest.TestCase):
     def test_plain_for(self):
         out = sum_for_loop(5)
         self.assertEqual(out, 1+2+3+4+5)
 
+    def test_generator_for(self):
+        out = generate_sequence(5)
+        self.assertListEqual(out, [1,2,3,4,5])
 
+        
 if __name__ == '__main__':
     unittest.main()
